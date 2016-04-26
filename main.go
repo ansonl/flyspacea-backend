5d420b69f9e419ddc61684285f6374b4bb40d10d
anson17 <ansonl@users.noreply.github.com>
2016-04-25 22:21:16 -0400

    update

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ type OCRHavenResp struct {
 }
 
 func main() {
-	resp, err := http.Get("https://graph.facebook.com/v2.5/AndersenPassengerTerminal/photos?type=uploaded&access_token=522755171230853%7ChxS9OzJ4I0CqmmrESRpNHfx77vs")
+	resp, err := http.Get("https://graph.facebook.com/v2.5/travispassengerterminal/photos?type=uploaded&access_token=522755171230853%7ChxS9OzJ4I0CqmmrESRpNHfx77vs")
 	if err != nil {
 		// handle error
 	}
@@ -45,9 +45,14 @@ func main() {
 	//fmt.Println(string(body))
 
     pagePhotos := Photos{}
-    json.Unmarshal(body, &pagePhotos)
+    pagePhotosErr := json.Unmarshal(body, &pagePhotos)
+    if pagePhotosErr != nil {
+    	fmt.Println("Error unmarshaling page photos edge.")
+    }
+    fmt.Println("Page photos edge retrieved.")
+    
 
-    	/*
+    /*
     res2B, _ := json.Marshal(pagePhotos)
     fmt.Println(string(res2B))
     */
@@ -72,7 +77,8 @@ func main() {
 		}
 		fmt.Println(timeOjb)
 		*/
-    	
+    	fmt.Println("Getting photo node.")
+
     	photoNode := fmt.Sprintf("https://graph.facebook.com/v2.5/%v?fields=source&access_token=522755171230853%%7ChxS9OzJ4I0CqmmrESRpNHfx77vs", v.Id)
     	//fmt.Println(photoNode)
     	resp, err := http.Get(photoNode)
@@ -83,7 +89,11 @@ func main() {
 		body, err := ioutil.ReadAll(resp.Body)
 
     	photo := Photo{}
-    	json.Unmarshal(body, &photo)
+    	photoNodeErr := json.Unmarshal(body, &photo)
+    	if (photoNodeErr != nil) {
+    		fmt.Println("Error unmarshaling photoNode")
+    	}
+    	fmt.Println("Photo node retrieved.")
 
     	/*
     	testPhotoNodeString, _ := json.Marshal(photo)
@@ -95,7 +105,7 @@ func main() {
 	    data := url.Values{}
 	    data.Set("apikey", "6f5569d3-8fc0-4c74-80b4-efe9fd4c90a0")
 	    data.Add("url", photo.Source)
-	    data.Add("mode", "document_photo")
+	    data.Add("mode", "document_scan")
 
 	    u, _ := url.ParseRequestURI(apiUrl)
 	    u.Path = resource
@@ -105,6 +115,8 @@ func main() {
 	    r, _ := http.NewRequest("POST", urlStr, bytes.NewBufferString(data.Encode())) // <-- URL-encoded payload
 	    r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	    r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	    fmt.Println("OCR request sent.")
 
 	    havenResp, _ := client.Do(r)
 	    fmt.Println(havenResp.Status)
@@ -129,11 +141,20 @@ func main() {
 	    */
 
 	    ocrHavenResp := OCRHavenResp{}
-	    json.Unmarshal(ocrOutputRaw, &ocrHavenResp)
-	    /*
+	    ocrErr := json.Unmarshal(ocrOutputRaw, &ocrHavenResp)
+	    if (ocrErr != nil) {
+	    	fmt.Println("Error unmarshaling OCR request.")
+	    }
+	    fmt.Println("OCR request received.")
+	    
 	    testOCRHavenRespString, _ := json.Marshal(ocrHavenResp)
     	fmt.Println(string(testOCRHavenRespString))
-    	*/
+    	
+
+    	if (len(ocrHavenResp.TextBlockArray) == 0) {
+    		fmt.Println("No text recognized.")
+    		continue
+    	}
 
     	//handle case to replace `1\n` but not `2016\n`
     	reNewLine := regexp.MustCompile(`(([^0-9][0-9])?\n)`)
@@ -141,17 +162,21 @@ func main() {
 
     	//ocrHavenResp.TextBlockArray[0].Text = strings.Replace(ocrHavenResp.TextBlockArray[0].Text, "\n", "", -1)
 
-    	rePhotoDate := regexp.MustCompile(`(?i:[0-9]{2} (?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-9]{4})`)
-	    fmt.Println(rePhotoDate.FindString(ocrHavenResp.TextBlockArray[0].Text))
+    	rePhotoDate := regexp.MustCompile(`(?i:[0-9]{2}[ ]*(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[ ]*[0-9Il]{2,4})`)
+	    fmt.Printf("Photo date %v\n", rePhotoDate.FindString(ocrHavenResp.TextBlockArray[0].Text))
 
 	    //fmt.Println(ocrHavenResp.TextBlockArray[0].Text)
 
-	    rePhotoDateToListing := regexp.MustCompile(`(?im:[0-9]{2} (?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-9]{4}.*?)[0-9]{4}`)    
-	    departureStartIndex := rePhotoDateToListing.FindStringIndex(ocrHavenResp.TextBlockArray[0].Text)[1] - 4;
+	    rePhotoDateToListing := regexp.MustCompile(`(?im:[0-9]{2}[ ]*(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)[ ]*[0-9]{2,4}.*?)[0-9]{4}`)    
+	   	returnStringIndex := rePhotoDateToListing.FindStringIndex(ocrHavenResp.TextBlockArray[0].Text)
+	   	if (len(returnStringIndex) > 1) {
+	   		departureStartIndex := returnStringIndex[1] - 4
+	   		reDeparture := regexp.MustCompile(`(([0-9]{4})[ ]*([A-z ,&;]*)?.*?(-[A-z ,&;]*)*[ ]*(?:([0-9]{1,3}[A-z])|TBD))`)
+	   		fmt.Printf("%q\n",reDeparture.FindAllStringSubmatch(ocrHavenResp.TextBlockArray[0].Text[departureStartIndex:], -1))
+	   	}
 	    //fmt.Println(ocrHavenResp.TextBlockArray[0].Text[departureStartIndex:])
 
-	    reDeparture := regexp.MustCompile(`(([0-9]{4})[ ]*([A-z ,&;]*)?.*?(-[A-z ,&;]*)*[ ]*([0-9]{1,3}[A-z]))`)
-	    fmt.Printf("%q\n",reDeparture.FindAllStringSubmatch(ocrHavenResp.TextBlockArray[0].Text[departureStartIndex:], -1))
+	    
 
     }
 }
